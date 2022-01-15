0490ac9210358476b43dda8d45f70fb6f6f4fa25
Ngoc Nguyen <ngocnguyenc3@gmail.com>
2022-01-15 12:52:53 +0700

    Sign up user(didnt fix status yet)

--- app/src/main/java/com/ashpex/portality/CreateCourseActivity.java
+++ app/src/main/java/com/ashpex/portality/CreateCourseActivity.java
@@ -3,12 +3,29 @@ package com.ashpex.portality;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CreateCourseActivity extends AppCompatActivity {
-
+    private ImageButton btnBack_course;
+    private EditText txtNameCourse;
+    private EditText txtTime;
+    private EditText txtDay_of_wwek;
+    private EditText txtTimeStart;
+    private EditText txtTimeEnd;
+    private EditText txtDes;
+    private EditText txtLink;
+    private EditText txtFee;
+    private EditText txtRequire;
+    private ImageButton btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
+        
+        mappingControls();
+    }
+
+    private void mappingControls() {
     }
 }
--- app/src/main/java/com/ashpex/portality/fragment/SignUpCourseFragment.java
+++ app/src/main/java/com/ashpex/portality/fragment/SignUpCourseFragment.java
@@ -1,6 +1,7 @@
 package com.ashpex.portality.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ashpex.portality.CreateCourseActivity;
 import com.ashpex.portality.R;
 import com.ashpex.portality.SignUpInterface;
 import com.ashpex.portality.adapter.SignUpCourseAdapter;
@@ -71,6 +73,13 @@ public class SignUpCourseFragment extends Fragment implements SignUpInterface {
 
     private void addEvents() {
         getAvailableCourse();
+        btnAddNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CreateCourseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void getAvailableCourse() {
