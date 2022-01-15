de31c60487e99b983df6d229ee4834d1099384b1
ashpex <ashpex@pm.me>
2022-01-15 19:20:26 +0700

    Fix calendar fragment

--- app/src/main/java/com/ashpex/portality/fragment/CalendarFragment.java
+++ app/src/main/java/com/ashpex/portality/fragment/CalendarFragment.java
@@ -3,17 +3,20 @@ package com.ashpex.portality.fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.ashpex.portality.R;
 import com.ashpex.portality.adapter.UserCourseOnStudyingAdapter;
@@ -46,6 +49,7 @@ public class CalendarFragment extends Fragment {
     private String userName;
     private String token;
     private Integer userId;
+    private int type;
     List<UserCourseOnStudying> mlist;
 
     @Nullable
@@ -55,9 +59,60 @@ public class CalendarFragment extends Fragment {
 
         view =  inflater.inflate(R.layout.fragment_calendar, container, false);
         mappingControls();
-        calendarSetup();
-
+        getData();
         return view;
+
+    }
+
+    private void calendar(List<UserCourseOnStudying> mlist, List<UserCourseOnStudying> weeklist){
+        Calendar minDate = Calendar.getInstance();
+        Calendar maxDate = Calendar.getInstance();
+        CalendarPickerController calendarPickerController = new CalendarPickerController() {
+            @Override
+            public void onDaySelected(DayItem dayItem) {
+
+            }
+
+            @Override
+            public void onEventSelected(CalendarEvent event) {
+
+            }
+
+            @Override
+            public void onScrollToDate(Calendar calendar) {
+
+            }
+        };
+
+
+        List<CalendarEvent> eventList = new ArrayList<>();
+
+        if(mlist.size() !=0 && mlist != null){
+            Calendar startTime3 = Calendar.getInstance();
+            startTime3.add(Calendar.DAY_OF_YEAR, 0);
+            Calendar endTime3 = Calendar.getInstance();
+            endTime3.add(Calendar.DAY_OF_YEAR, 0);
+            filterList(mlist);
+            for(int i = 0; i < mlist.size(); i++){
+                BaseCalendarEvent event3 = new BaseCalendarEvent(mlist.get(i).getCourse_name(),mlist.get(i).getTeacher_name(),mlist.get(i).getTeacher_name(),
+                        R.color.darker_blue, startTime3, endTime3, true);
+                eventList.add(event3);
+            }
+        }
+
+        Calendar startTime2 = Calendar.getInstance();
+        startTime2.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar endTime2 = Calendar.getInstance();
+        endTime2.add(Calendar.DAY_OF_YEAR, 1);
+        filterList(weeklist,startTime2);
+        if (weeklist.size() != 0 && weeklist != null){
+            BaseCalendarEvent event2 = new BaseCalendarEvent(weeklist.get(0).getCourse_name(),weeklist.get(0).getTeacher_name(),weeklist.get(0).getTeacher_name(),
+                    R.color.darker_blue, startTime2, endTime2, true);
+            eventList.add(event2);
+        }
+
+
+        mAgendaCalendarView.init(eventList, minDate, maxDate, Locale.getDefault(), calendarPickerController);
 
     }
 
@@ -106,10 +161,16 @@ public class CalendarFragment extends Fragment {
 
                     if(mlist.size() !=0){
 
-
+                        /*
                         for(int i = 0; i < mlist.size(); i++){
                             BaseCalendarEvent event = new BaseCalendarEvent(mlist.get(i).getCourse_name(),mlist.get(i).getTeacher_name(),mlist.get(i).getTeacher_name(),
                                     Color.parseColor(mlist.get(i).getColor()), startTime, endTime, true);
+                            eventList.add(event);
+                        }
+                        */
+                        for(int i = 0; i < mlist.size(); i++){
+                            BaseCalendarEvent event = new BaseCalendarEvent(mlist.get(i).getCourse_name(),mlist.get(i).getTeacher_name(),mlist.get(i).getTeacher_name(),
+                                    R.color.darker_blue, startTime, endTime, true);
                             eventList.add(event);
                         }
 
@@ -228,17 +289,29 @@ public class CalendarFragment extends Fragment {
 
     }
 
-    private void getData(){
+    private void getData() {
         SharedPreferences sharedPref = view.getContext().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
         userName = sharedPref.getString("user_name", "null");
         token = sharedPref.getString("token", "null");
         userId = sharedPref.getInt("user_id", 0);
+        type = sharedPref.getInt("user_type", 0);
+        if(type == 1) {
+        }
 
         ApiService.apiService.getUserCourse(userId, token).enqueue(new Callback<List<UserCourseOnStudying>>() {
             @Override
             public void onResponse(Call<List<UserCourseOnStudying>> call, Response<List<UserCourseOnStudying>> response) {
                 if(response.code() == 200) {
-                    mlist = response.body();
+                    List<UserCourseOnStudying> mlist = response.body();
+                    List<UserCourseOnStudying> tempList = new ArrayList<>();
+
+                    for (UserCourseOnStudying item : mlist) {
+                        tempList.add(item);
+                    }
+                    List<UserCourseOnStudying> weeklist = new ArrayList<>(tempList);
+                    CalendarFragment.FilterAsyncTask filterAsyncTask = new CalendarFragment.FilterAsyncTask();
+                    filterAsyncTask.setMlist(mlist,weeklist);
+                    filterAsyncTask.execute();
                 }
             }
 
@@ -247,13 +320,46 @@ public class CalendarFragment extends Fragment {
                 Log.d("alo", "Cant connect");
             }
         });
+    }
 
+
+
+    class FilterAsyncTask extends AsyncTask<Void, Void, Void> {
+        List<UserCourseOnStudying> mlist;
+        List<UserCourseOnStudying> weeklist;
+
+        public void setMlist(List<UserCourseOnStudying> mlist, List<UserCourseOnStudying> weeklist) {
+            this.mlist = mlist;
+            this.weeklist = weeklist;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            filterList(mlist);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            if(mlist.size() !=0) {
+                calendar(mlist,weeklist);
+            }
+            else
+                calendar(mlist,weeklist);
+                Toast.makeText(getContext(), "Hôm nay lịch trống", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void filterList(List<UserCourseOnStudying> mlist) {
+        Date currentTime = Calendar.getInstance().getTime();
+        int day = currentTime.getDay();
+        mlist.removeIf(i -> i.getDay_study() != day);
     }
 
     private void filterList(List<UserCourseOnStudying> mlist, Calendar calendarInstance) {
         Date currentTime = calendarInstance.getTime();
         int day = currentTime.getDay() ;
-        mlist.removeIf(i -> i.getDay_study() == day);
+        mlist.removeIf(i -> i.getDay_study() != day);
     }
 
     private void mappingControls() {
